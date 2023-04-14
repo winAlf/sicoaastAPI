ea5bf04e5ac6d50b36419eb3e63b6fe951ff102c
FPozos <FPozos@FPOZOSHOME>
2023-04-14 11:05:07 -0600

    prueba

--- sicoaastAPI/Models/Dtos/Usuario/UsuarioLoginRespuestaDto.cs
+++ sicoaastAPI/Models/Dtos/Usuario/UsuarioLoginRespuestaDto.cs
@@ -2,7 +2,7 @@
 {
     public class UsuarioLoginRespuestaDto
     {
-       public Usuario Usuario { get; set; } 
+       public UsuarioDto Usuario { get; set; } 
        public string Token { get; set; }
     }
 }
